e037b07e07115e55799e83917e091d58a87aad9d
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-06-26 22:03:24 +0600

    base দিয়ে শুধু Constructor না, Method-ও Call করা যায় er ag porjonto

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
+        
         public class Student : Person
         {
             public double Marks;
+
+            public Student(string name, int id, double marks): base(name,id)  // Person-এর constructor-কে name আর id পাঠাচ্ছে
+            {
+                this.Marks = marks;   // Student শুধু নিজের Marks handle করছে
+            }
+
             public string GetGrade()
             {
                 if (Marks >= 80) return "A+";
@@ -15,6 +22,7 @@
 
         }
 
+        /*
         public class Teacher
         {
             public string? Name;
@@ -31,6 +39,7 @@
                 return $"{Name} Sir ({Subject})";
             }
         }
+        */
 
         public class Person
         {
@@ -48,9 +57,37 @@
                 Console.WriteLine($"Name: {Name}, ID: {Id}");
             }
         }
+        
+
+
+
+        /*
+        public class Animal
+        {
+            public string Name;
+
+            public Animal(string name)
+            {
+                this.Name = name;
+                Console.WriteLine("Constructor of Animal Run");
+            }
+        }
+
+        public class Dog: Animal
+        {
+            public string Breed;
+
+            public Dog(string name, string breed) : base(name)
+            {
+                this.Breed = breed;
+                Console.WriteLine("Constructor of Dog Run");
+            }
+        }
+        */
 
         static void Main(string[] args)
         {
+            /*
             Student rahim = new Student("Rahim", 101);
             rahim.Marks = 85;
 
@@ -59,6 +96,19 @@
             Console.WriteLine(rahim.Marks);       // Output: 85 — Student-এর নিজের
             Console.WriteLine(rahim.GetGrade());  // Output: A+ — Student-এর নিজের
             rahim.PrintInfo();                    // Output: Name: Rahim, ID: 101 — Person থেকে
+            */
+
+            //  Dog tommy = new Dog("Tommy", "Labrador");
+
+            Student rahim = new Student("Rahim", 101, 85);
+
+            Console.WriteLine(rahim.Name);       // Output: Rahim — Person-এর constructor set করেছে
+            Console.WriteLine(rahim.Id);         // Output: 101 — Person-এর constructor set করেছে
+            Console.WriteLine(rahim.Marks);      // Output: 85 — Student-এর constructor set করেছে
+            Console.WriteLine(rahim.GetGrade()); // Output: A+ — Student-এর নিজের method
+            rahim.PrintInfo();                   // Output: Name: Rahim, ID: 101 — Person থেকে পাওয়া method
+
+
 
         }
     }
