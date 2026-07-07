b88850deee443d383e5a727cf35be515516c2b8f
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-07-07 10:33:52 +0600

    পর্ব ০৬: Real Project — School Management System   Step 2: Person Abstract Class

--- Real Project — School Management System - OOP/Program.cs
+++ Real Project — School Management System - OOP/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Real_Project___School_Management_System___OOP
+﻿using Microsoft.VisualBasic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Real_Project___School_Management_System___OOP
 {
     internal class Program
     {
@@ -6,6 +9,57 @@
         {
             void SendNotification(string message);
         }
+
+        public abstract class Person
+        {
+            private string name;
+            private int id;
+
+           public string Name
+            {
+                get { return name; }
+
+                set
+                {
+                    if(string.IsNullOrEmpty(value))
+                    {
+                        Console.WriteLine("Name can't be Empty");
+                        return;
+                    }
+                    name = value;
+                }
+            }
+
+            public int Id
+            {
+                get { return id; }
+                set
+                {
+                    if (value <= 0)
+                    {
+                        Console.WriteLine("ID must be a positive integer.");
+                        return;
+                    }
+                    id = value;
+                }
+            }
+
+            public Person(string name, int id)
+            {
+                this.name = name;
+                this.id = id;
+            }
+
+            public abstract void PrintInfo();
+
+            public void SayHello()
+            {
+                Console.WriteLine($"Hello, my name is {name} and my ID is {id}.");
+            }
+
+        }
+
+
 
 
         static void Main(string[] args)
