12052778c854acf115d9c348a297d7f4a6ea7afd
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-07-07 08:25:06 +0600

    পর্ব ০৬: Real Project — School Management System Start Work

---
+++ Real Project — School Management System - OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Real_Project___School_Management_System___OOP
+{
+    internal class Program
+    {
+        public interface INotifiable
+        {
+            void SendNotification(string message);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
